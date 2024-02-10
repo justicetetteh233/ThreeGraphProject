f6a007ff797382af6a10c8f337f9abb5f3f2d87c
justice <justice@DESKTOP-3NGKVBS.localdomain>
2024-02-10 23:50:14 +0000

    upto second user input

--- Game_engine/GameLogic.py
+++ Game_engine/GameLogic.py
@@ -45,6 +45,54 @@ class GameLogic:
         if self.firstUserInpu_is_tValid(c1,o1):
             return o1
     
-       
+    def SinglePositionTargets(self,position):
+        targets = []
+        if position%2 == 1:
+            targets.append((position -1)%8)
+            targets.append((position+1)%8)
+            targets.append((position+4)%8)
+            targets.append(8)
 
-      
+        if position != 8 and position%2 == 0:
+            targets.append((position -1)%8)
+            targets.append((position+1)%8)
+            targets.append((position -2)%8)
+            targets.append((position+2)%8)
+            targets.append((position+4)%8)
+            targets.append(8)
+
+        if position == 8:
+            for i in range(0,8):
+                targets.append(i)
+
+        return targets
+    
+    def is_win(self,FP, SP, TP):
+        winmatirx  = [
+         [0,1,2],[2,3,4],[4,5,6],[6,7,0],[0,8,4],[1,8,5],[2,8,6],[3,4,7]
+         ]
+        
+        if FP != SP and FP != TP  and SP != TP:
+            for wm in winmatirx:
+                if SP in wm  and FP in wm  and TP in wm:
+                    return True
+                
+        return False
+    
+    def secondComputerMove(self,c1,o1):
+        targets  = self.SinglePositionTargets(c1)
+        if o1 in targets:
+            for i in range(0,9):
+                if self.is_win(c1,o1,i):
+                    od =  i
+                    break
+            targets.remove(o1)
+            targets.remove(od)
+        return targets[random.randint(0,targets.__len__()-1)]
+    
+    def takeUserSecondInput(self,c1,o1,c2):
+        o2  = int(input("enter you move "))
+        while not self.firstUserInpu_is_tValid(c1,o2) or not self.firstUserInpu_is_tValid(o1,o2) or not self.firstUserInpu_is_tValid(c2,o2):
+            o2  = int(input("enter you move "))
+        if self.firstUserInpu_is_tValid(c1,o2) and self.firstUserInpu_is_tValid(c2,o2)  and  self.firstUserInpu_is_tValid(o1,o2):
+            return o2
--- main.py
+++ main.py
@@ -13,3 +13,16 @@ print(Start.displayMatrixState(o1,o2,o3,c1,c2,c3))
 
 o1 = Start.TakeUserFirstInput(c1)
 print(Start.displayMatrixState(o1,o2,o3,c1,c2,c3))
+
+c2 = Start.secondComputerMove(c1,o1)
+print(Start.displayMatrixState(o1,o2,o3,c1,c2,c3))
+
+
+o2 = Start.takeUserSecondInput(c1,o1,c2)
+print(Start.displayMatrixState(o1,o2,o3,c1,c2,c3))
+
+
+
+# print(Start.is_win(6,4,4))
+# print(Start.SinglePositionTargets(4))
+# print(Start.secondComputerMove(4,3))
